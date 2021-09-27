6fdbd3108775687c304e392a7585604a5c792f09
DKBoston15 <46634008+DKBoston15@users.noreply.github.com>
2021-09-27 18:27:20 -0500

    Home Page spacing adjustments

--- components/Meetings.tsx
+++ components/Meetings.tsx
@@ -1,4 +1,4 @@
-// import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"
 // import axios from "axios";
 // import { InlineWidget } from "react-calendly";
 // import date from "date-and-time";
