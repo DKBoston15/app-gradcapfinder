19d319db52004b10a256c80c953473cdc56b3ab3
DKBoston15 <dakotabrownag@gmail.com>
2021-09-28 17:47:35 -0500

    Project Name Check

--- components/TaskComponents/Tasks.tsx
+++ components/TaskComponents/Tasks.tsx
@@ -1,56 +1,57 @@
-import React, { useEffect } from "react"
-import Checkbox from "./Checkbox"
-import { useTasks } from "../../hooks"
-import { collatedTasks } from "../../constants"
-import { getTitle, getCollatedTitle, collatedTasksExist } from "../../helpers"
-import { useSelectedProjectValue, useProjectsValue } from "../../context"
-import { AddTask } from "./AddTask"
+import React, { useEffect } from "react";
+import Checkbox from "./Checkbox";
+import { useTasks } from "../../hooks";
+import { collatedTasks } from "../../constants";
+import { getTitle, getCollatedTitle, collatedTasksExist } from "../../helpers";
+import { useSelectedProjectValue, useProjectsValue } from "../../context";
+import { AddTask } from "./AddTask";
 
 export const Tasks = () => {
-    const { selectedProject } = useSelectedProjectValue()
-    const { projects } = useProjectsValue()
-    const { tasks } = useTasks(selectedProject)
+  const { selectedProject } = useSelectedProjectValue();
+  const { projects } = useProjectsValue();
+  const { tasks } = useTasks(selectedProject);
 
-    let projectName = ""
+  let projectName = "";
 
-    if (collatedTasksExist(selectedProject) && selectedProject) {
-        projectName = getCollatedTitle(collatedTasks, selectedProject).name
-    }
+  if (collatedTasksExist(selectedProject) && selectedProject) {
+    projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+  }
 
-    if (
-        projects &&
-        projects.length > 0 &&
-        selectedProject &&
-        !collatedTasksExist(selectedProject)
-    ) {
-        projectName = getTitle(projects, selectedProject).name
-    }
+  if (
+    projects &&
+    projects.length > 0 &&
+    projects.hasOwnProperty("name") &&
+    selectedProject &&
+    !collatedTasksExist(selectedProject)
+  ) {
+    projectName = getTitle(projects, selectedProject).name;
+  }
 
-    useEffect(() => {
-        document.title = `${projectName}`
-    })
+  useEffect(() => {
+    document.title = `${projectName}`;
+  });
 
-    return (
-        <div className="p-8" data-testid="tasks">
-            <h2 data-testid="project-name" className="mb-8">
-                {projectName}
-            </h2>
+  return (
+    <div className="p-8" data-testid="tasks">
+      <h2 data-testid="project-name" className="mb-8">
+        {projectName}
+      </h2>
 
-            <ul className="space-y-4">
-                {tasks.map((task) => (
-                    // @ts-ignore
-                    <div key={`${task.id}`}>
-                        <li className="flex space-x-4 items-center">
-                            {/* @ts-ignore */}
-                            <Checkbox id={task.id} taskDesc={task.task} />
-                            {/* @ts-ignore */}
-                            <span>{task.task}</span>
-                        </li>
-                        <hr className="border-1 border-gray-300" />
-                    </div>
-                ))}
-            </ul>
-            <AddTask />
-        </div>
-    )
-}
+      <ul className="space-y-4">
+        {tasks.map((task) => (
+          // @ts-ignore
+          <div key={`${task.id}`}>
+            <li className="flex space-x-4 items-center">
+              {/* @ts-ignore */}
+              <Checkbox id={task.id} taskDesc={task.task} />
+              {/* @ts-ignore */}
+              <span>{task.task}</span>
+            </li>
+            <hr className="border-1 border-gray-300" />
+          </div>
+        ))}
+      </ul>
+      <AddTask />
+    </div>
+  );
+};
