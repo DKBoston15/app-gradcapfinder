aee2e726280bc8c046ccacb465e545ab2206fe78
DKBoston15 <46634008+DKBoston15@users.noreply.github.com>
2021-09-27 21:15:42 -0500

    Task Fixing

--- components/Pages/Dashboard.tsx
+++ components/Pages/Dashboard.tsx
@@ -1,30 +1,34 @@
-import React, { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import React, { useState, useEffect } from "react"
+import { motion, AnimatePresence } from "framer-motion"
+import { format, addDays } from "date-fns"
 // Components
-import MainNav from "../MainNav";
-import HomePanel from "../HomePanel";
-import Tasks from "../Pages/Tasks";
-import Chat from "../Pages/Chat";
-import Documents from "../Pages/Documents";
-import VideoLibrary from "../Pages/VideoLibrary";
+import MainNav from "../MainNav"
+import HomePanel from "../HomePanel"
+import Tasks from "../Pages/Tasks"
+import Chat from "../Pages/Chat"
+import Documents from "../Pages/Documents"
+import VideoLibrary from "../Pages/VideoLibrary"
 
 interface IDashboardProps {
-  user: any;
+    user: any
 }
 
 export default function Dashboard({ user }: IDashboardProps) {
-  const [currentPage, setCurrentPage] = useState("Dashboard");
+    const [currentPage, setCurrentPage] = useState("Dashboard")
 
-  return (
-    <div>
-      <div className="flex min-h-screen">
-        <MainNav currentPage={currentPage} setCurrentPage={setCurrentPage} />
-        {currentPage === "Dashboard" && <HomePanel user={user} />}
-        {currentPage === "Tasks" && <Tasks />}
-        {currentPage === "Chat" && <Chat />}
-        {currentPage === "Documents" && <Documents />}
-        {currentPage === "VideoLibrary" && <VideoLibrary />}
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <div className="flex min-h-screen">
+                <MainNav
+                    currentPage={currentPage}
+                    setCurrentPage={setCurrentPage}
+                />
+                {currentPage === "Dashboard" && <HomePanel user={user} />}
+                {currentPage === "Tasks" && <Tasks />}
+                {currentPage === "Chat" && <Chat />}
+                {currentPage === "Documents" && <Documents />}
+                {currentPage === "VideoLibrary" && <VideoLibrary />}
+            </div>
+        </div>
+    )
 }
