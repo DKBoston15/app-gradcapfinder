637fc8f0414c7d23a0a36a8a56e213cdca37e45d
DKBoston15 <dakotabrownag@gmail.com>
2021-09-28 17:33:44 -0500

    Project List Styling

--- components/TaskComponents/IndividualProject.tsx
+++ components/TaskComponents/IndividualProject.tsx
@@ -4,10 +4,34 @@ import { useProjectsValue, useSelectedProjectValue } from "../../context";
 import firebase from "../../firebase";
 import Modal from "./Modal";
 
-export default function IndividualProject({ project }: any) {
+export default function IndividualProject({ project, active, index }: any) {
   const [showConfirm, setShowConfirm] = useState(false);
   const { projects, setProjects } = useProjectsValue();
   const { setSelectedProject } = useSelectedProjectValue();
+
+  function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  const colorKey = [
+    "#eb4d4b",
+    "#6ab04c",
+    "#95afc0",
+    "#686de0",
+    "#e056fd",
+    "#f9ca24",
+    "#130f40",
+    "#4834d4",
+    "#22a6b3",
+    "#ffbe76",
+    "#686de0",
+    getRandomColor(),
+  ];
 
   const deleteProject = (docId: any) => {
     firebase
@@ -23,13 +47,21 @@ export default function IndividualProject({ project }: any) {
 
   return (
     <>
-      <div className="flex flex-col">
+      <div
+        className={`flex flex-col group hover:bg-white cursor-pointer rounded-md px-4 py-2 ${
+          active == project.projectId ? "bg-white" : ""
+        }`}
+      >
         <div className="flex justify-between">
-          <div className="space-x-2">
-            <span>•</span>
+          <div className="space-x-2 flex items-center">
+            <div
+              className="rounded-full w-2 h-2"
+              style={{ backgroundColor: colorKey[index] }}
+            />
             <span>{project.name}</span>
           </div>
           <span
+            className="hidden group-hover:flex flex items-center"
             data-testid="delete-project"
             onClick={() => {
               setShowConfirm(!showConfirm);
--- components/TaskComponents/Projects.tsx
+++ components/TaskComponents/Projects.tsx
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import { useSelectedProjectValue, useProjectsValue } from "../../context";
 import IndividualProject from "./IndividualProject";
 
-export default function Projects({ activeValue = true }) {
+export default function Projects({ activeValue = null }) {
   const [active, setActive] = useState(activeValue);
   const { setSelectedProject } = useSelectedProjectValue();
   const { projects } = useProjectsValue();
   return (
     projects &&
-    projects.map((project: any) => (
+    projects.map((project: any, index: number) => (
       <li
         key={project.projectId}
         data-doc-id={project.docId}
@@ -18,7 +18,7 @@ export default function Projects({ activeValue = true }) {
           setSelectedProject(project.projectId);
         }}
       >
-        <IndividualProject project={project} />
+        <IndividualProject project={project} active={active} index={index} />
       </li>
     ))
   );
