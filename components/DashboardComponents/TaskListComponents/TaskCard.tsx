2decf36f7e65e457354342f661b7270aa6270cc9
DKBoston15 <dakotabrownag@gmail.com>
2021-11-12 09:03:50 -0600

    tasks due soon

--- components/DashboardComponents/TaskListComponents/TaskCard.tsx
+++ components/DashboardComponents/TaskListComponents/TaskCard.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 export default function TaskCard({ task }: any) {
   return (
-    <div className="bg-white rounded-lg h-full w-full mr-4">
-      <div>{task.task}</div>
-      <div>{task.date}</div>
+    <div className="bg-white rounded-lg h-full w-full col-span-1 shadow-md p-2 flex flex-col justify-between hover:transform hover:scale-105">
+      <div className="font-bold">{task.task}</div>
+      <div>
+        <span className="text-gray">Due Date:</span> {task.date}
+      </div>
     </div>
   );
 }
--- components/DashboardComponents/TaskListComponents/TasksDue.tsx
+++ components/DashboardComponents/TaskListComponents/TasksDue.tsx
@@ -6,6 +6,7 @@ import TaskCard from "./TaskCard";
 export default function TasksDue({ setCurrentPage }: any) {
   const [user, loading, error] = useAuthState(firebase.auth());
   const [tasks, setTasks] = useState([]);
+  const [taskCount, setTaskCount] = useState();
   function byField(fieldName: any) {
     // @ts-ignore
     return (a, b) => (a[fieldName] > b[fieldName] ? 1 : -1);
@@ -14,15 +15,11 @@ export default function TasksDue({ setCurrentPage }: any) {
   useEffect(() => {
     if (user) {
       const getTasks = async () => {
-        let tasks = firebase
-          .firestore()
-          .collection("tasks")
-          .where("userId", "==", user.uid);
-
         const tasksRef = firebase
           .firestore()
           .collection("tasks")
-          .where("userId", "==", user.uid);
+          .where("userId", "==", user.uid)
+          .limit(8);
         const snapshot = await tasksRef.get();
         const taskArray: any = [];
         snapshot.forEach((doc) => {
@@ -36,8 +33,9 @@ export default function TasksDue({ setCurrentPage }: any) {
             taskArray.push(doc.data());
           }
         });
+
+        setTaskCount(taskArray.length);
         setTasks(taskArray.sort(byField("date")));
-        console.log(taskArray);
       };
       getTasks();
     }
@@ -46,15 +44,26 @@ export default function TasksDue({ setCurrentPage }: any) {
   return (
     <div className="bg-dashGray w-1/2 rounded-xl p-5">
       <div className="flex justify-between w-full">
-        <span className="font-semibold text-2xl">Tasks due soon</span>{" "}
+        <span className="font-semibold text-2xl flex items-center">
+          <span>Tasks due soon</span>
+          <span className="ml-2 text-sm bg-primary text-white rounded-full px-2 py-1">
+            {taskCount}
+          </span>
+        </span>
         <button
-          className={`font-bold text-black rounded-lg py-2 px-4 my-1 mr-1 text-md cursor-pointer bg-white`}
+          className={`font-bold text-black rounded-lg py-2 px-4 my-1 mr-1 text-md cursor-pointer bg-white hover:bg-primary hover:text-white hover:transition hover:ease-in hover:duration-200 hover:scale-105`}
           onClick={() => setCurrentPage("Tasks")}
         >
           View All
         </button>
       </div>
-      <div className="flex h-3/5 justify-between mt-8">
+      {tasks.length === 0 && (
+        <div className="flex flex-col items-center mt-20 text-xl font-semibold">
+          <div>You are all caught up!</div>
+          <div>No tasks due in the future!</div>
+        </div>
+      )}
+      <div className="grid grid-cols-2 gap-4 h-4/5 mt-4">
         {tasks.length > 0 && tasks.map((task: any) => <TaskCard task={task} />)}
       </div>
     </div>
