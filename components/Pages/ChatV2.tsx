595881bc2d35e2a86ea33483da4c7d2e2930b2af
DKBoston15 <dakotabrownag@gmail.com>
2022-02-10 13:07:56 -0600

    build fix

--- components/Pages/ChatV2.tsx
+++ components/Pages/ChatV2.tsx
@@ -41,16 +41,19 @@ export default function ChatV2({ setCurrentPage }: any) {
   useEffect(async () => {
     await getDiscussionsForAdmin();
     await getDiscussionsForUser();
-    await addDefaultDiscussions(
-      user?.id,
-      process.env.NEXT_PUBLIC_DANE_USER_ID,
-      "Chat with Dr.Bozeman"
-    );
-    await addDefaultDiscussions(
-      user?.id,
-      process.env.NEXT_PUBLIC_TECH_USER_ID,
-      "Chat with Tech Support"
-    );
+    if (discussionsForAdmin.length == 0) {
+      console.log("Creating default discussions");
+      await addDefaultDiscussions(
+        user?.id,
+        process.env.NEXT_PUBLIC_DANE_USER_ID,
+        "Chat with Dr.Bozeman"
+      );
+      await addDefaultDiscussions(
+        user?.id,
+        process.env.NEXT_PUBLIC_TECH_USER_ID,
+        "Chat with Tech Support"
+      );
+    }
     if (discussionsForUser && discussionsForAdmin) {
       try {
         if (isAdmin()) {
