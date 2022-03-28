48b70fd009f939f7b80679878f08745bc2498005
DKBoston15 <dakotabrownag@gmail.com>
2022-03-28 08:40:04 -0500

    Added new project stores

---
+++ src/stores/figureStore.ts
@@ -0,0 +1,55 @@
+import create from "zustand";
+import { supabase } from "../supabase/index";
+
+export const useFigureStore = create<any>((set) => ({
+  figures: [],
+  getFigures: async (selectedProject: any) => {
+    const user = supabase.auth.user();
+    const data = await supabase
+      .from("figures")
+      .select("*")
+      .eq("user_id", user?.id)
+      .eq("project_id", selectedProject)
+      .order("title", { ascending: true })
+      .then(({ data, error }) => {
+        if (!error) {
+          // @ts-ignore
+          set({ articles: data });
+          return data;
+        }
+      });
+    return data;
+  },
+  addFigure: async (
+    userId: string,
+    title: string,
+    link: string,
+    selectedProject: number
+  ) => {
+    const user = supabase.auth.user();
+    const { error } = await supabase.from("figures").insert([
+      {
+        link,
+        title,
+        user_id: userId,
+        project_id: selectedProject,
+      },
+    ]);
+    const getFigures = useFigureStore.getState().getFigures;
+    if (selectedProject) {
+      getFigures(selectedProject);
+    }
+  },
+  deleteFigure: async (id: number) => {
+    const { error } = await supabase.from("figures").delete().eq("id", id);
+  },
+  editFigure: async (id: number, title: string, link: string) => {
+    const { data, error } = await supabase
+      .from("figures")
+      .update({
+        title,
+        link,
+      })
+      .eq("id", id);
+  },
+}));
---
+++ src/stores/modelStore.ts
@@ -0,0 +1,55 @@
+import create from "zustand";
+import { supabase } from "../supabase/index";
+
+export const useModelStore = create<any>((set) => ({
+  models: [],
+  getModels: async (selectedProject: any) => {
+    const user = supabase.auth.user();
+    const data = await supabase
+      .from("models")
+      .select("*")
+      .eq("user_id", user?.id)
+      .eq("project_id", selectedProject)
+      .order("title", { ascending: true })
+      .then(({ data, error }) => {
+        if (!error) {
+          // @ts-ignore
+          set({ articles: data });
+          return data;
+        }
+      });
+    return data;
+  },
+  addModel: async (
+    userId: string,
+    title: string,
+    link: string,
+    selectedProject: number
+  ) => {
+    const user = supabase.auth.user();
+    const { error } = await supabase.from("models").insert([
+      {
+        link,
+        title,
+        user_id: userId,
+        project_id: selectedProject,
+      },
+    ]);
+    const getModels = useModelStore.getState().getModels;
+    if (selectedProject) {
+      getModels(selectedProject);
+    }
+  },
+  deleteModel: async (id: number) => {
+    const { error } = await supabase.from("models").delete().eq("id", id);
+  },
+  editModel: async (id: number, title: string, link: string) => {
+    const { data, error } = await supabase
+      .from("models")
+      .update({
+        title,
+        link,
+      })
+      .eq("id", id);
+  },
+}));
