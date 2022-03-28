0c4eeb7bbcb050621331fa99d7fe161411ec7dc6
DKBoston15 <dakotabrownag@gmail.com>
2022-03-27 19:29:28 -0500

    Article & Entry Delete Functionality on Project Deletion

--- src/components/Projects/ProjectOverviewHeader/SplitAddProjectButton/SplitAddProjectButton.tsx
+++ src/components/Projects/ProjectOverviewHeader/SplitAddProjectButton/SplitAddProjectButton.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { SplitButton } from "primereact/splitbutton";
 import { confirmDialog } from "primereact/confirmDialog";
 import AddProjectDialog from "../AddProjectDialog/AddProjectDialog";
